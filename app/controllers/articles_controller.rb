13e807d58ddc7404088901fe1025d849dbd42903
raphaelsattler <raphaelsattler@hotmail.com>
2018-08-07 15:31:54 -0300

    Implement listing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.all
+  end
 
   def new
     @article = Article.new
@@ -11,7 +13,7 @@ class ArticlesController < ApplicationController
       flash[:success] = 'Article has been created'
       redirect_to articles_path
     else
-      flash[:danger] = 'Article has not been created'
+      flash.now[:danger] = 'Article has not been created'
       render :new
     end
   end
