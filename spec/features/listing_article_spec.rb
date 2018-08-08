01dc39568f6cb76c160f25781d6e97b3fe7db80b
raphaelsattler <raphaelsattler@hotmail.com>
2018-08-08 14:45:30 -0300

    Fix broken editing article spec

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.feature 'Listing Articles' do
   before do
+    john = User.create(email: 'john@example.com', password: 'password')
+    login_as(john)
     @article1 = Article.create(title: 'The first article',
-    body: 'Lorem ipsum dolor sit amet, consectetur.')
+    body: 'Lorem ipsum dolor sit amet, consectetur.', user: john)
     @article2 = Article.create(title: 'The second article',
-    body: 'Pellentesque ac ligula in tellus feugiat.')
+    body: 'Pellentesque ac ligula in tellus feugiat.', user: john)
   end
 
   scenario 'A user lists all articles' do
